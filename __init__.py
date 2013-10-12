08558fdb85c8d5d03d3e17bc5af7e78a134b31bc
James Spencer <j.spencer@imperial.ac.uk>
2013-10-12 17:46:13 +0100

    Hack to force python 2 and python 3 compatibility.

--- __init__.py
+++ __init__.py
@@ -5,7 +5,13 @@ __version__ = '0.1'
 __author__  = 'Psi4 Developer'
 
 # Load Python modules
-from pymodule import *
+# Something weird with boost_python, but the explicit import doesn't work on
+# python 2 (at least, not on CMTH machines) whereas the relative import doesn't
+# work on python 3 (i.e. my laptop).
+try:
+    from fcidump.pymodule import *
+except ImportError:
+    from pymodule import *
 
 # Load C++ plugin
 import os

6d7e9126891575cbd87490d5a2bfb7237266b8c6
James Spencer <j.spencer@imperial.ac.uk>
2016-01-31 19:59:46 +0000

    Update to play nicely with latest psi4 and document compilation.

--- pymodule.py
+++ pymodule.py
@@ -1,3 +1,27 @@
+#
+#@BEGIN LICENSE
+#
+# fcidump by James Spencer, a plugin to:
+#
+# PSI4: an ab initio quantum chemistry software package
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#@END LICENSE
+#
+
 import psi4
 import re
 import os
@@ -8,7 +32,7 @@ from driver import *
 from wrappers import *
 from molutil import *
 import p4util
-from psiexceptions import *
+from p4xcpt import *
 
 
 def run_fcidump(name, **kwargs):
@@ -24,8 +48,8 @@ def run_fcidump(name, **kwargs):
     # Your plugin's psi4 run sequence goes here
     scf_helper(name, **kwargs)
     returnvalue = psi4.plugin('fcidump.so')
+    psi4.set_variable('CURRENT ENERGY', returnvalue)
 
-    return returnvalue
 
 # Integration with driver routines
 procedures['energy']['fcidump'] = run_fcidump
